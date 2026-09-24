b22860d8d0bd53a43d9f054bf87ac8909e305e06
kviernes255lfi-a11y <kviernes255.lfi@gmail.com>
2026-09-24 11:31:56 +0800

    Update seed.py

--- app/seed.py
+++ app/seed.py
@@ -207,7 +207,7 @@ def seed_if_empty():
         InventoryItem(name="DUNKIN YAN PILLOW-STRAWBERRY", category="PROMO ITEM", unit="BOX"),
         InventoryItem(name="DUNKIN YAN PILLOW-CHOCOLATE", category="PROMO ITEM", unit="PCS"),
         InventoryItem(name='23" UMBRELLA WITH 12 PANELS (MAGENTA)', category="PROMO ITEM", unit="PCS"),
-        InventoryItem(name="23" UMBRELLA WITH 12 PANELS (ORANGE)", category="PROMO ITEM", unit="PCS"),
+        InventoryItem(name='23" UMBRELLA WITH 12 PANELS (ORANGE)', category="PROMO ITEM", unit="PCS"),
         InventoryItem(name="2026 BWS POGI CARD 1", category="PROMO ITEM", unit="PCS"),
         InventoryItem(name="BIG BALLOONS 12" W/LOGO", category="PROMO ITEM", unit="PCS"),
         InventoryItem(name="SMALL BALLOONS 6"", category="PROMO ITEM", unit="PCS"),
